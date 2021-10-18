a3a65d61e7911344387cf03abc68abaaf42ca846
Marco Herrera <marcoh1012@gmail.com>
2021-10-17 20:52:04 -0500

    add light control

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from settings import url, api_token
-from rpi_ws281x import Color
+from rpi_ws281x import PixelStrip, Color
 from lights import colorWipe
 import time
 import requests
 import json
+
+
+
+
+# LED strip configuration:
+LED_COUNT = 60        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Create NeoPixel object with appropriate configuration.
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
 
 
 def getPrinterStatus(): 
@@ -19,11 +38,11 @@ def getPrinterStatus():
 
 def setColor(state):
     if state == 'Operational':
-        colorWipe(color=Color(0,255,0))
+        colorWipe(strip, color=Color(0,255,0))
         # Set to color Green
         print('connected')
     elif state == 'Printing':
-        colorWipe(color=Color(0,0,255))
+        colorWipe(strip, color=Color(0,0,255))
         # Set to Green color flashing
         print('printing')
     elif state == 'Paused' or state == 'Pausing':
